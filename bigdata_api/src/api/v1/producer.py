5aa277b2a7e7ac086aaf6d4522656942583af566
aximon <aximon@yandex.ru>
2022-03-13 16:17:24 +0300

    Добавлена проверка авторизации

--- bigdata_api/src/api/v1/producer.py
+++ bigdata_api/src/api/v1/producer.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Header, HTTPException, Request
 
-# from dependencies.authentication import get_token, make_request
+from dependencies.authentication import get_token, make_request
 from models.producer import ProducerMessage, ProducerResponse
 from services.producer import ProducerService, get_producer_service
 
 router = APIRouter()
+
+
+async def check_views_statistics_permission(token=get_token(), x_request_id=Header(None)):
+    await make_request(permission='views_statistic', token=token, x_request_id=x_request_id)
 
 
 @router.post(
@@ -14,9 +18,12 @@ router = APIRouter()
     description='Send massage to kafka',
     response_description='Topicname and message id',
 )
-async def kafka_produce(msg: ProducerMessage, topicname: str,
-                        producer_service: ProducerService = Depends(get_producer_service),
-                        ):
+async def kafka_produce(
+        msg: ProducerMessage,
+        topicname: str,
+        producer_service: ProducerService = Depends(get_producer_service),
+        allowed: bool = Depends(check_views_statistics_permission),
+) -> ProducerResponse:
     await producer_service.send(topicname, msg)
     response = ProducerResponse(
         message_id=msg.message_id, topic=topicname
